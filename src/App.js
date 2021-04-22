de316fac7c31c71ac7712503830d8a8836b9b1bd
dma150 <dma150@uw.edu>
2021-04-22 10:53:29 -0700

    saved new header

--- src/App.js
+++ src/App.js
@@ -5,22 +5,14 @@ const viewWidth = 500;
 
 
 const App = () => {
+    fetch("https://raw.githubusercontent.com/dma150/react-parcel-example-1/main/weather.csv")
+    .then(response => response.json())
+    .then(data => console.log(data));
+
     return (
-        <svg 
-            style={{border: "1px solid lightgrey", width: viewWidth,
-            height: viewHeight}}
-        >
-            <circle cx={20} cy={20} r="5" />
-            <rect x={200} y={200} width={10} height={10} fill="rgb(230, 230, 230)"/>
-            <rect x={212} y={200} width={10} height={10} fill="rgb(230, 230, 230)"/>
-            <rect x={224} y={200} width={10} height={10} fill="rgb(230, 230, 230)"/>
-            <rect x={236} y={200} width={10} height={10} fill="rgb(230, 230, 230)"/>
-            <rect x={248} y={200} width={10} height={10} />
-            <line x1={20} y1={viewHeight - 20} x2={150} y2={100} stroke="black" />
-            <text x="20" y="35">
-                Price history of 100 randomly selected Pokemon cards!!!
-            </text>
-        </svg>
+        <div>
+            <h1>Exploratory Data Analysis, Assignment 2, INFO 474 SP 2021</h1>
+        </div>
     ); // outer brace = js / inner brace = object
 };
 
